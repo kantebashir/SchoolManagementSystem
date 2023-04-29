a5b192c2406de912a987dd9b07a2846a8aa06288
Ogole <ogolefadilhussen1@gmail.com>
2023-04-29 10:45:28 +0300

    Created all the neccesary functionalities

---
+++ schoolmanagement/studentsmanagementapp/views.py
@@ -0,0 +1,232 @@
+import json
+import requests
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render, reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.core.files.storage import FileSystemStorage
+from .EmailBackend import EmailBackend
+from .models import Attendance, Session, Subject
+from .forms import *
+# Create your views here.
+
+def home_page(request):
+    if request.user.is_authenticated:
+        return redirect('login_page')
+    return render(request,'school/index.html')
+
+def teacherclick(request):
+    return render(request, 'school/teacherclick.html')
+
+#teacher approval
+def teacher_waits_approval(request):
+    return render(request, 'school/teacher_wait_for_approval.html')
+
+
+#student approval
+def student_waits_approval(request):
+    return render(request, 'school/student_wait_for_approval.html')
+
+
+def student_signup_page(request):
+    student_form = StudentForm(request.POST or None, request.FILES or None)
+    context = {'form': student_form, 'page_title': 'Add Student'}
+    if request.method == 'POST':
+        if student_form.is_valid():
+            first_name = student_form.cleaned_data.get('first_name')
+            last_name = student_form.cleaned_data.get('last_name')
+            address = student_form.cleaned_data.get('address')
+            email = student_form.cleaned_data.get('email')
+            gender = student_form.cleaned_data.get('gender')
+            password = student_form.cleaned_data.get('password')
+            course = student_form.cleaned_data.get('course')
+            session = student_form.cleaned_data.get('session')
+            passport = request.FILES['profile_pic']
+            fs = FileSystemStorage()
+            filename = fs.save(passport.name, passport)
+            passport_url = fs.url(filename)
+            try:
+                user = CustomUser.objects.create_user(
+                    email=email, password=password, user_type=3, first_name=first_name, last_name=last_name, profile_pic=passport_url)
+                user.gender = gender
+                user.address = address
+                user.student.session = session
+                user.student.course = course
+                user.save()
+                messages.success(request, "Application succesful")
+                return redirect(reverse('student_waits_approval'))
+            except Exception as e:
+                messages.error(request, "Could Not Add: " + str(e))
+        else:
+            messages.error(request, "Could Not Add: ")
+    return render(request, 'school/studentsignup.html', context)
+
+
+
+def teacher_signup(request):
+    form = StaffForm(request.POST or None, request.FILES or None)
+    context = {'form': form, 'page_title': 'Add Staff'}
+    if request.method == 'POST':
+        if form.is_valid():
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            address = form.cleaned_data.get('address')
+            email = form.cleaned_data.get('email')
+            gender = form.cleaned_data.get('gender')
+            password = form.cleaned_data.get('password')
+            course = form.cleaned_data.get('course')
+            passport = request.FILES.get('profile_pic')
+            fs = FileSystemStorage()
+            filename = fs.save(passport.name, passport)
+            passport_url = fs.url(filename)
+            try:
+                user = CustomUser.objects.create_user(
+                    email=email, password=password, user_type=2, first_name=first_name, last_name=last_name, profile_pic=passport_url)
+                user.gender = gender
+                user.address = address
+                user.staff.course = course
+                user.save()
+                messages.success(request, "registration Successfully pending admin approval ")
+                return redirect(reverse('teacher_waits_approval'))
+
+            except Exception as e:
+                messages.error(request, "Could Not register " + str(e))
+        else:
+            messages.error(request, "Please fulfil all requirements")
+
+    return render(request, 'school/teachersignup.html', context)
+
+
+
+
+
+    
+# def student_signup_page(request):
+#     form = StudentForm()
+#     if request.method=='POST':
+#         form = StudentForm(request.POST)
+#         if form.is_valid():
+#             user=form.save()
+#             user.set_password(user.password)
+#             user.save()
+#             #user = form.cleaned_data.get('email')
+#             my_student_group = Group.objects.get_or_create(user_type='3')
+#             my_student_group[0].user_set.add(user)
+#         return redirect('login_page')
+#     context = {'form':form}
+#     return render(request,'school/studentsignup.html',context)
+
+
+def login_page(request):
+    if request.user.is_authenticated:
+        if request.user.user_type == '1':
+            return redirect(reverse("admin_home"))
+        elif request.user.user_type == '2':
+            return redirect(reverse("staff_home"))
+        else:
+            return redirect(reverse("student_home"))
+    return render(request, 'main_app/login.html')
+
+
+def doLogin(request, **kwargs):
+    if request.method != 'POST':
+        return HttpResponse("<h4>Denied</h4>")
+    else:
+        #Google recaptcha
+        captcha_token = request.POST.get('g-recaptcha-response')
+        captcha_url = "https://www.google.com/recaptcha/api/siteverify"
+        captcha_key = "6LfswtgZAAAAABX9gbLqe-d97qE2g1JP8oUYritJ"
+        data = {
+            'secret': captcha_key,
+            'response': captcha_token
+        }
+        # Make request
+        try:
+            captcha_server = requests.post(url=captcha_url, data=data)
+            response = json.loads(captcha_server.text)
+            if response['success'] == False:
+                messages.error(request, 'Invalid Captcha. Try Again')
+                return redirect('login_page')
+        except:
+            messages.error(request, 'Captcha could not be verified. Try Again')
+            return redirect('login_page')
+        
+        #Authenticate
+        user = EmailBackend.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
+        if user != None:
+            login(request, user)
+            if user.user_type == '1':
+                return redirect(reverse("admin_home"))
+            elif user.user_type == '2':
+                return redirect(reverse("staff_home"))
+            else:
+                return redirect(reverse("student_home"))
+        else:
+            messages.error(request, "Invalid details")
+            return redirect("login_page")
+
+
+
+def logout_user(request):
+    if request.user != None:
+        logout(request)
+    return redirect("login_page")
+
+
+@csrf_exempt
+def get_attendance(request):
+    subject_id = request.POST.get('subject')
+    session_id = request.POST.get('session')
+    try:
+        subject = get_object_or_404(Subject, id=subject_id)
+        session = get_object_or_404(Session, id=session_id)
+        attendance = Attendance.objects.filter(subject=subject, session=session)
+        attendance_list = []
+        for attd in attendance:
+            data = {
+                    "id": attd.id,
+                    "attendance_date": str(attd.date),
+                    "session": attd.session.id
+                    }
+            attendance_list.append(data)
+        return JsonResponse(json.dumps(attendance_list), safe=False)
+    except Exception as e:
+        return None
+
+
+def showFirebaseJS(request):
+    data = """
+    // Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/7.22.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.22.1/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+    apiKey: "AIzaSyBarDWWHTfTMSrtc5Lj3Cdw5dEvjAkFwtM",
+    authDomain: "sms-with-django.firebaseapp.com",
+    databaseURL: "https://sms-with-django.firebaseio.com",
+    projectId: "sms-with-django",
+    storageBucket: "sms-with-django.appspot.com",
+    messagingSenderId: "945324593139",
+    appId: "1:945324593139:web:03fa99a8854bbd38420c86",
+    measurementId: "G-2F2RXTL9GT"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function (payload) {
+    const notification = JSON.parse(payload);
+    const notificationOption = {
+        body: notification.body,
+        icon: notification.icon
+    }
+    return self.registration.showNotification(payload.notification.title, notificationOption);
+});
+    """
+    return HttpResponse(data, content_type='application/javascript')
